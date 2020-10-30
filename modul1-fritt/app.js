21ec93c32327dbe8a18ed3578228729be4dac2f3
mohche16 <70907380+mohche16@users.noreply.github.com>
2020-10-30 13:34:16 +0100

    Add files via upload

---
+++ modul1-fritt/app.js
@@ -0,0 +1,102 @@
+// apikey=dcb7b7b4 / #dcb7b7b4 efter index.html i urlen
+const apiKey = location.hash.substring(1);
+const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&t=`;
+
+//Fetch movie name by title
+let fetchMovieDataTitle = async (moviename) => {
+  let dataMovie = {};
+  //Fetch single object movie (not an array)
+  await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=` + moviename)
+    .then((response) => response.json())
+    .then((data) => (dataMovie = data));
+  return dataMovie;
+};
+
+//Fetch 10 random movies
+let fetchMovieData = async () => {
+  let dataMovie = {};
+  //Fetch array of objects (movies)
+  await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=movie`)
+    .then((response) => response.json())
+    .then((data) => (dataMovie = data.Search));
+  return dataMovie;
+};
+
+//Parse over every array item to insert HTML with proper values.
+function parseArrayToMovieList(arraytoparse) {
+  let parsedArray = arraytoparse
+    .map((index) => {
+      return `
+        <div class="movie" id=${index.Title}>
+        <ul>
+          <h3>Movie Title:</h3>
+          <h5 id="title">${index.Title}</h5>
+          <h3>Year:</h3>
+          <h5 id="year">${index.Year}</h5>
+          <h3>imdbID:</h3>
+          <a href="https://www.imdb.com/title/${index.imdbID}">
+            <h5 id="imdbID">${index.imdbID}</h5></a
+          >
+          <h3>Type:</h3>
+          <h5 id="Type">Movie</h5>
+          <img
+            id="poster"
+            src=${index.Poster}
+          />
+        </ul>
+      </div>`;
+    })
+    .join("");
+  return parsedArray;
+}
+
+//HTML elements for movie container div and searchbox
+const moviesContainerEl = document.querySelector(".movieslist");
+const searchboxEl = document.querySelector(".searchbox-movie");
+
+window.onload = async () => {
+  //Fetch movies array
+  let movieData = await fetchMovieData();
+  console.log(movieData);
+  //Parse array to include html
+  let parsedArray = await parseArrayToMovieList(movieData);
+  // set container html to html with all movies loaded.
+  moviesContainerEl.innerHTML = parsedArray;
+};
+
+searchboxEl.addEventListener("input", async (e) => {
+  //our searched movie
+  let searchBoxValue = e.target.value;
+  //if value is nothing then reset to default movie values.
+  if (!searchBoxValue) {
+    let movieData = await fetchMovieData();
+    let parsedArray = await parseArrayToMovieList(movieData);
+    moviesContainerEl.innerHTML = parsedArray;
+    return;
+  }
+  //else fetch data and set innerhtml to parsed data with single object (movie searched for)
+  let movieData = await fetchMovieDataTitle(searchBoxValue);
+
+  if (movieData.Title) {
+    let parsedArray = `
+          <div class="movie" id=${movieData.Title}>
+          <ul>
+            <h3>Movie Title:</h3>
+            <h5 id="title">${movieData.Title}</h5>
+            <h3>Year:</h3>
+            <h5 id="year">${movieData.Year}</h5>
+            <h3>imdbID:</h3>
+            <a href="https://www.imdb.com/title/${movieData.imdbID}">
+              <h5 id="imdbID">${movieData.imdbID}</h5></a
+            >
+            <h3>Type:</h3>
+            <h5 id="Type">Movie</h5>
+            <img
+              id="poster"
+              src=${movieData.Poster}
+            />
+          </ul>
+        </div>`;
+    moviesContainerEl.innerHTML = parsedArray;
+  }
+});
